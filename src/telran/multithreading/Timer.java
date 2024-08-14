a48610b50a9252ff0aa026732e41574cab99c519
PaulT1983 <pault83@gmail.com>
2024-08-14 14:27:56 +0300

    HW#45 Solution.

--- src/telran/multithreading/Timer.java
+++ src/telran/multithreading/Timer.java
@@ -1,22 +1,46 @@
 package telran.multithreading;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 public class Timer extends Thread {
-//TODO displaying time in a given format and a given resolution
-	//example displaying each second, or each 5 seconds, etc.
-	public Timer() {
-		setDaemon(true);
-	}
-	public void run() {
-		while(true) {
-			System.out.println(LocalTime.now());
-			try {
-				sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+	// displaying time in a given format and a given resolution
+    // example displaying each second, or each 5 seconds, etc.
+    // Provide an option to set the end time for the timer
+    private final long resolutionMillis;
+    private final DateTimeFormatter formatter;
+    private final LocalTime endTime;
+    private boolean running = true; // Flag to control the loop execution
+
+    // Default constructor
+    public Timer() {
+        setDaemon(true);
+        this.resolutionMillis = 1000; // Default to 1 second
+        this.formatter = DateTimeFormatter.ofPattern("HH:mm:ss"); // Default format
+        this.endTime = LocalTime.MAX; // Default to no specific end time
+    }
+
+    // Advanced constructor
+    public Timer(long resolution, TimeUnit resolutionUnit, String timeFormat, 
+                 long endDelay, TimeUnit endUnit) {
+        setDaemon(true);
+        this.resolutionMillis = resolutionUnit.toMillis(resolution);
+        this.formatter = DateTimeFormatter.ofPattern(timeFormat);
+        this.endTime = LocalTime.now().plusNanos(endUnit.toNanos(endDelay));
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                System.out.println(LocalTime.now().format(formatter));
+                sleep(resolutionMillis);
+                if (LocalTime.now().isAfter(endTime)) {
+                    running = false;
+               }
+            } catch (InterruptedException e) {
+            	e.printStackTrace();
+            }
+        }
+    }
 }
--- src/telran/multithreading/TimerControllerAppl.java
+++ src/telran/multithreading/TimerControllerAppl.java
@@ -1,16 +1,22 @@
 package telran.multithreading;
 
-public class TimerControllerAppl {
+import java.util.concurrent.TimeUnit;
 
-	public static void main(String[] args) throws InterruptedException {
-		Timer timer = new Timer();
-		timer.start();
-		Thread.sleep(10000);
-		//TODO
+public class TimerControllerAppl {
+    public static void main(String[] args) throws InterruptedException {
 		//figure out a solution allowing timer stop
 		//as example in 5 seconds you stop the timer
 		//following 5 seconds application is running with no timer
-
-	}
-
+    	
+        // Using advanced constructor with resolution and end delay
+        long resolution = 1;
+        TimeUnit resolutionUnit = TimeUnit.SECONDS;
+        String timeFormat = "HH:mm:ss.SSS";
+        long endDelay = 10;
+        TimeUnit endUnit = TimeUnit.SECONDS;
+        Timer advancedTimer = new Timer(resolution, resolutionUnit, timeFormat, endDelay, endUnit);
+        advancedTimer.start();
+        Thread.sleep(18000);
+        // The main thread will finish after this sleep, causing the daemon timer thread to stop automatically.
+    }
 }
